f1fea712fac6fecbc3cbb9311156248d8fc0a2c8
William Okafor <wilpat456@gmail.com>
2020-04-08 06:32:02 +0100

    Implements setup, layout and movie index page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,97 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container mx-auto px-4 pt-16">
+        <div class="popular-movies">
+            <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">Popular Movies</h2>
+            <div class="grid grid-cols-5 gap-8">
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/parasite.jpg" alt="Parasite" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray:300">Parasite</a>
+                        <div class="flex items-center text-gray-400 text-sm mt-1">
+                            <svg class="fill-current text-orange-500 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18"><path d="M9 11.3l3.71 2.7-1.42-4.36L15 7h-4.55L9 2.5 7.55 7H3l3.71 2.64L5.29 14z"></path><path fill="none" d="M0 0h18v18H0z"></path></svg>
+                            <span class="ml-1">85%</span>
+                            <span class="mx-2">|</span>
+                            <span>Apr 08, 2020</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Action, Thriller, Comedy
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/joker.jpg" alt="Parasite" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray:300">Joker</a>
+                        <div class="flex items-center text-gray-400 text-sm mt-1">
+                            <svg class="fill-current text-orange-500 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18"><path d="M9 11.3l3.71 2.7-1.42-4.36L15 7h-4.55L9 2.5 7.55 7H3l3.71 2.64L5.29 14z"></path><path fill="none" d="M0 0h18v18H0z"></path></svg>
+                            <span class="ml-1">85%</span>
+                            <span class="mx-2">|</span>
+                            <span>Apr 08, 2020</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Action, Thriller, Comedy
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/sonic.jpg" alt="Parasite" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray:300">Sonic</a>
+                        <div class="flex items-center text-gray-400 text-sm mt-1">
+                            <svg class="fill-current text-orange-500 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18"><path d="M9 11.3l3.71 2.7-1.42-4.36L15 7h-4.55L9 2.5 7.55 7H3l3.71 2.64L5.29 14z"></path><path fill="none" d="M0 0h18v18H0z"></path></svg>
+                            <span class="ml-1">85%</span>
+                            <span class="mx-2">|</span>
+                            <span>Apr 08, 2020</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Action, Thriller, Comedy
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/frozen2.jpg" alt="Parasite" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray:300">Frozen 2</a>
+                        <div class="flex items-center text-gray-400 text-sm mt-1">
+                            <svg class="fill-current text-orange-500 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18"><path d="M9 11.3l3.71 2.7-1.42-4.36L15 7h-4.55L9 2.5 7.55 7H3l3.71 2.64L5.29 14z"></path><path fill="none" d="M0 0h18v18H0z"></path></svg>
+                            <span class="ml-1">85%</span>
+                            <span class="mx-2">|</span>
+                            <span>Apr 08, 2020</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Action, Thriller, Comedy
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/parasite.jpg" alt="Parasite" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray:300">Parasite</a>
+                        <div class="flex items-center text-gray-400 text-sm mt-1">
+                            <svg class="fill-current text-orange-500 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18"><path d="M9 11.3l3.71 2.7-1.42-4.36L15 7h-4.55L9 2.5 7.55 7H3l3.71 2.64L5.29 14z"></path><path fill="none" d="M0 0h18v18H0z"></path></svg>
+                            <span class="ml-1">85%</span>
+                            <span class="mx-2">|</span>
+                            <span>Apr 08, 2020</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Action, Thriller, Comedy
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+@endsection
